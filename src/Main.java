f60ec6a0690a2e2d572c9dfe92f4ecb8f0f27dee
nadsay <nadsatyyy@gmail.com>
2023-09-02 18:49:34 +0300

    Домашка 11, Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         task1();
+        task2();
     }
+
+    //Задача 1
 
     public static void findLeapYear(int leapYear) {
         if (leapYear % 4 == 0) {
@@ -17,5 +21,29 @@ public class Main {
 
         int year = 2024;
         findLeapYear(year);
+    }
+
+    //Задача 2
+
+    public static void whichVersion(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+
+        if (clientOS == 0 && clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке \n ");
+        } else  if (clientOS == 0 && clientDeviceYear == currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке \n");
+        } else if (clientOS == 1 && clientDeviceYear < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке \n");
+        } else  if (clientOS == 1 && clientDeviceYear == currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке \n");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("\n Задача 2 \n");
+
+        int deviceYear = 2023;
+        int deviceOS = 0;
+        whichVersion(deviceOS, deviceYear);
     }
 }
